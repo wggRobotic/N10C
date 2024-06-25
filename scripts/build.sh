4b2e08504427277d4d95ebfb20027e75f3f505c9
Felix Schreiber <f.schreiber.2000@web.de>
2024-06-25 17:07:59 +0200

    fixed parameters

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,4 @@
 cd ../..
 colcon build --packages-select n10c --cmake-args=-DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=ON
 . ./install/setup.sh
-ros2 run n10c n10c
+ros2 run n10c n10c --ros-args -p image0:=/image_raw
--- src/n10c.cpp
+++ src/n10c.cpp
@@ -13,21 +13,28 @@ void N10C::SetupWithImageTransport(image_transport::ImageTransport &it)
 {
   rclcpp::Parameter image0, image1, image2, barcode, velocity, enable;
 
-  auto has_image0 = get_parameter("image0", image0);
-  auto has_image1 = get_parameter("image1", image1);
-  auto has_image2 = get_parameter("image2", image2);
-  auto has_barcode = get_parameter("barcode", barcode);
-  auto has_velocity = get_parameter("twist", velocity);
-  auto has_enable = get_parameter("enable", enable);
+  declare_parameter("image0", "/n10/image0");
+  declare_parameter("image1", "/n10/image1");
+  declare_parameter("image2", "/n10/image2");
+  declare_parameter("barcode", "/n10/barcode");
+  declare_parameter("twist", "/n10/twist");
+  declare_parameter("enable", "/n10/enable");
 
-  m_ImageSubscriber0 = it.subscribe(has_image0 ? image0.as_string() : "/n10/image0", 10, &N10C::ImageCallback0, this);
-  m_ImageSubscriber1 = it.subscribe(has_image1 ? image1.as_string() : "/n10/image1", 10, &N10C::ImageCallback1, this);
-  m_ImageSubscriber2 = it.subscribe(has_image2 ? image2.as_string() : "/n10/image2", 10, &N10C::ImageCallback2, this);
+  get_parameter("image0", image0);
+  get_parameter("image1", image1);
+  get_parameter("image2", image2);
+  get_parameter("barcode", barcode);
+  get_parameter("twist", velocity);
+  get_parameter("enable", enable);
 
-  m_BarcodeSubscriber = create_subscription<std_msgs::msg::String>(has_barcode ? barcode.as_string() : "/n10/barcode", 10, std::bind(&N10C::BarcodeCallback, this, std::placeholders::_1));
+  m_ImageSubscriber0 = it.subscribe(image0.as_string(), 10, &N10C::ImageCallback0, this);
+  m_ImageSubscriber1 = it.subscribe(image1.as_string(), 10, &N10C::ImageCallback1, this);
+  m_ImageSubscriber2 = it.subscribe(image2.as_string(), 10, &N10C::ImageCallback2, this);
 
-  m_TwistPublisher = create_publisher<geometry_msgs::msg::Twist>(has_velocity ? velocity.as_string() : "/n10/velocity", 10);
-  m_EnableMotorClient = create_client<std_srvs::srv::SetBool>(has_enable ? enable.as_string() : "/n10/enable");
+  m_BarcodeSubscriber = create_subscription<std_msgs::msg::String>(barcode.as_string(), 10, std::bind(&N10C::BarcodeCallback, this, std::placeholders::_1));
+
+  m_TwistPublisher = create_publisher<geometry_msgs::msg::Twist>(velocity.as_string(), 10);
+  m_EnableMotorClient = create_client<std_srvs::srv::SetBool>(enable.as_string());
 
   m_Timer = create_wall_timer(20ms, std::bind(&N10C::TimerCallback, this));
 }
