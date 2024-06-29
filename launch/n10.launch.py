591c9a6c85c35fa6b05ce49ff7daa8ee22e56624
Corvus006 <wagneraron@outlook.de>
2024-06-29 14:20:42 +0200

    added launch file

---
+++ launch/n10.launch.py
@@ -0,0 +1,32 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='n10c',
+            executable='n10c',
+            parameters=[
+                {'image0': '/n10/intel/color'},
+                {'image1': '/n10/rear/color'},
+                {'image2': '/n10/cam_dif'}
+            ],
+            output={
+                'stdout': 'screen',
+                'stderr': 'screen'
+            }
+        ),
+        Node(
+            package= 'n10_cam_dif',
+            executable= 'cam_dif',
+        ),
+        Node(
+            package='zbar_ros',
+            executable='barcode_reader',
+            name= 'n10_barcode_reader',
+            remappings=[
+                ('/image', '/n10/intel/color'),
+                ('/barcode','/n10/barcode')],
+            output='screen'
+        )
+    ])
--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,4 @@
 cd ../..
 colcon build --packages-select n10c --cmake-args=-DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=ON
 . ./install/setup.sh
-ros2 run n10c n10c --ros-args -p image0:=/image_raw -p enable:=/eduard/enable
+ros2 run n10c n10c --ros-args  -p twist:=/eduard/vel/teleop -p enable:=/eduard/enable
