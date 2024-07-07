753b171acb795733d5e52fcf3ea64891b781039b
Felix Schreiber <f.schreiber.2006@proton.me>
2024-07-07 20:33:32 +0200

    made launch file a yaml one

--- scripts/build.bash
+++ scripts/build.bash
@@ -3,4 +3,4 @@
 cd ../..
 colcon build --packages-select n10c --cmake-args=-DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=ON
 . ./install/local_setup.bash
-ros2 launch n10c n10.launch.py
+ros2 launch n10c n10.launch.yaml
